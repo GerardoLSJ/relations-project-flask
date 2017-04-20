c86e80c9879b3642cece6d1bd8616308d4dca573
Sebastián Silva García <sebastian@Sebastians-MacBook-Air.local>
2017-04-19 22:25:39 -0500

    Displaying error codes

--- static/controllers.js
+++ static/controllers.js
@@ -2,6 +2,7 @@ app.controller('MainCtrl', function ($scope, $http) {
     console.log('MainCtrl')
     $scope.states = [];
     $scope.inputBoxes = [];
+    $scope.errorBoxes = [];
     $scope.inputCounter = 0;
     $scope.htmlString = '';
 
@@ -46,6 +47,22 @@ app.controller('MainCtrl', function ($scope, $http) {
                 console.error(e)
             })
     }
+
+    $scope.updatedPair = function(index){
+        //Comprueba que los elementos existen en el universo
+        var universe = $scope.universe.split(',')
+        var pair = $scope.inputBoxes[index].split(',')
+        if(universe.indexOf(pair[0]) == -1 || universe.indexOf(pair[1]) == -1){
+            console.log("EL ELEMENTO NO EXISTE")
+            $scope.errorBoxes[index] = "Alguno de los elementos no pertenece al universo";
+        }else{
+            $scope.errorBoxes[index] = '';
+        }
+        if(pair.length > 2){
+            $scope.errorBoxes[index] = "Ingresa sólo una pareja ordenada con el formato a,b"
+        }
+    }
+
 
 
 })
