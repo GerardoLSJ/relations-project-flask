ce421161987781b59e231e86fc92c35ab4660390
lsjgerardo <lsjgerardo@gmail.com>
2017-04-21 18:51:00 -0500

    clean conde and add clear btn

--- static/controllers.js
+++ static/controllers.js
@@ -1,26 +1,26 @@
 app.controller('MainCtrl', function ($scope, $http) {
     console.log('MainCtrl')
     $scope.states = [];
-    $scope.inputBoxes = [];
+    $scope.inputBoxes = ['', ''];
     $scope.errorBoxes = [];
     $scope.inputCounter = 0;
     $scope.htmlString = '';
+    $scope.universe = "1,2,3,hola"
 
-    $scope.addPair = function(){
+    $scope.addPair = function () {
         $scope.inputBoxes.push('');
         console.log($scope.inputBoxes)
-                console.log("Universe: " + $scope.universe)
-
     };
+
     $scope.getResults = function () {
         var myJson = []
-        console.log("Universe: " + $scope.universe)
         myJson.push($scope.universe);
-
-        var universe = myJson[0].split(',')
-        if (universe.length > 6) {
-            return alert('Menos de 6 elementos por favor')
-        }
+        /*
+                var universe = myJson[0].split(',')
+                if (universe.length > 6) {
+                    return alert('Menos de 6 elementos por favor')
+                }
+        */
         angular.forEach($scope.inputBoxes, function (element) {
             if (element) {
                 myJson.push(element);
@@ -28,16 +28,7 @@ app.controller('MainCtrl', function ($scope, $http) {
         });
 
         console.info(myJson)
-        console.log(universe)
 
-/*        
-        for(let i = 1; i<myJson.length; i++){
-            let pair = myJson[i].split(",")
-            for(let j = 0; j<universe.length; j++){
-              //  if(pair[0])
-            }
-        }
-*/
         $http.post('/api', myJson)
             .then((res) => {
                 console.info(res.data)
@@ -48,19 +39,28 @@ app.controller('MainCtrl', function ($scope, $http) {
             })
     }
 
-    $scope.updatedPair = function(index){
+    $scope.updatedPair = function (index) {
         //Comprueba que los elementos existen en el universo
         var universe = $scope.universe.split(',')
         var pair = $scope.inputBoxes[index].split(',')
-        if(universe.indexOf(pair[0]) == -1 || universe.indexOf(pair[1]) == -1){
+        if (universe.indexOf(pair[0]) == -1 || universe.indexOf(pair[1]) == -1) {
             console.log("EL ELEMENTO NO EXISTE")
             $scope.errorBoxes[index] = "Alguno de los elementos no pertenece al universo";
-        }else{
+        } else {
             $scope.errorBoxes[index] = '';
         }
-        if(pair.length > 2){
+        if (pair.length > 2) {
             $scope.errorBoxes[index] = "Ingresa sólo una pareja ordenada con el formato a,b"
         }
+    }
+
+    $scope.clearAll = function () {
+        $scope.states = [];
+        $scope.inputBoxes = ['', ''];
+        $scope.errorBoxes = [];
+        $scope.inputCounter = 0;
+        $scope.htmlString = '';
+        $scope.universe = "1,2,3,hola"
     }
 
 
